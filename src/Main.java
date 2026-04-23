87e9313cac9325a8aa89f636307849d59693e83c
Ivan Mordas <ivanmordas49@gmail.com>
2026-04-23 21:05:41 +0300

    Added Selected Sort

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new Random().ints(10, 0, 10).toArray();
+        int[] arr = new Random().ints(5, 0, 10).toArray();
         int target = new Random().nextInt(0, 10);
 
 //        Arrays.sort(arr);
-        bubbleSort(arr);
+//        bubbleSort(arr);
+
+        int[] arrStatic = {6, 5, 2, 8, 3,7};
+        selectionSort(arr);
 
         int resultLinearSearch = linearSearch(arr, target);
         int resultBinarySearch = binarySearch(arr, target);
@@ -62,10 +65,33 @@ public class Main {
                     isSwapped = true;
                 }
             }
-            if (!isSwapped){
+            if (!isSwapped) {
                 break;
             }
-                System.out.println("Step number " + i + ": " + Arrays.toString(arr));
+            System.out.println("Step number " + i + ": " + Arrays.toString(arr));
         }
+    }
+
+    private static void selectionSort(int[] arr) {
+        System.out.println("Array before sorting : " + Arrays.toString(arr));
+        int index = 0;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            int max = arr[0];
+            index = 0;
+            for (int j = 1; j <= arr.length - 1 - i; j++) {
+                if (max < arr[j]) {
+                    max = arr[j];
+                    index = j;
+                }
+            }
+            if (index != arr.length - 1 - i) {
+                int temp = arr[index];//8
+                arr[index] = arr[arr.length - 1 - i];
+                arr[arr.length - 1 - i] = temp;
+            }
+
+            System.out.println("Step number " + i + ": " + Arrays.toString(arr));
+        }
+        System.out.println("The result is : " + Arrays.toString(arr));
     }
 }
