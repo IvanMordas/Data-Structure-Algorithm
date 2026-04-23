60ddd13026fd81dc282c4d8d2a59c200f9541ba0
Ivan Mordas <ivanmordas49@gmail.com>
2026-04-23 15:45:29 +0300

    DUBSS - 04 Added Binary Search

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,20 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int[] arr = new Random().ints(10, 0, 10).toArray();
-        int target = new Random().nextInt(0, 10);
-        int result = linearSearch(arr, target);
+//        int[] arr = {1, 3, 3, 5, 5, 6, 7, 8, 9, 9};
+        int target = 5;
+//        int target = new Random().nextInt(0, 10);
+
+        Arrays.sort(arr);
+
+        int resultLinearSearch = linearSearch(arr, target);
+        int resultBinarySearch = binarySearch(arr, target);
 
         System.out.println("Array is  " + Arrays.toString(arr));
         System.out.println("Target element is " + target);
 
-        if (result != -1) {
-            System.out.println("The index of searching element is " + result);
+        if (resultBinarySearch != -1) {
+            System.out.println("The index of searching element is " + resultBinarySearch);
         } else {
             System.out.println("Element not found");
         }
@@ -21,6 +27,25 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
                 return i;
+            }
+        }
+        return -1;
+    }
+
+    private static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int index = (start + end) / 2;
+
+            if (arr[index] == target) {
+                return index;
+            }
+
+            if (arr[index] > target) {
+                end = index - 1;
+            } else {
+                start = index + 1;
             }
         }
         return -1;
