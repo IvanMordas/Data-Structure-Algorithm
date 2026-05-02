0b43ff0615ab462d800d6b0e44df7f20213eb67b
Ivan Mordas <ivanmordas49@gmail.com>
2026-05-02 14:31:08 +0300

    Added small changes in main

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,11 @@
-
-import binaryTree.BinaryTree;
-import sort.MergeSort;
-
 import java.util.Arrays;
-import java.util.Random;
-
-//import java.util.Stack;
-
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        int[] arr = new Random().ints(5, 0, 10).toArray();
-        int target = new Random().nextInt(0, 10);
-        int[] arrStatic = {5, 9, 7, 3, 6};
-
-        System.out.println("Before Sorting   " + Arrays.toString(arrStatic));
-
-        MergeSort.mergeSort(arrStatic, 0, arrStatic.length - 1);
-
-        System.out.println("After sorting:  " + Arrays.toString(arrStatic));
-
-        BinaryTree tree = new BinaryTree();
-        tree.insert(8);
-        tree.insert(7);
-        tree.insert(12);
-        tree.insert(15);
-        tree.insert(2);
-        tree.insert(5);
-        tree.inorder();
-
-
-
+    public static void main(String[] args) throws InterruptedException {
+        List<Integer> nums = Arrays.asList(4, 5, 7, 2);
+        StringBuffer sb = new StringBuffer("Java Code");
+        nums.stream().filter(n -> n >= 5).forEach(n -> System.out.print(n));
+        nums.forEach(System.out::println);
     }
-
 }
