908cb0a0ed9417ee3d43e44565b26e8fde039f22
Ivan Mordas <ivanmordas49@gmail.com>
2026-05-10 09:55:23 +0300

    Implemented LinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
+import linkedlist.LinkedList;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        List<Integer> nums = Arrays.asList(4, 5, 7, 2);
-        StringBuffer sb = new StringBuffer("Java Code");
-        nums.stream().filter(n -> n >= 5).forEach(n -> System.out.print(n));
-        nums.forEach(System.out::println);
+//        List<Integer> nums = Arrays.asList(4, 5, 7, 2);
+//        StringBuffer sb = new StringBuffer("Java Code");
+//        nums.stream().filter(n -> n >= 5).forEach(n -> System.out.print(n));
+//        nums.forEach(System.out::println);
+
+        LinkedList linkedList = new LinkedList(1);
+        linkedList.append(5);
+        linkedList.append(7);
+        linkedList.append(9);
+        linkedList.append(3);
+
+        System.out.println(linkedList.insert(1,2));
+
+        linkedList.printList();
     }
 }
--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -1,8 +1,169 @@
 package linkedlist;
 
 public class LinkedList {
-    Node head = null;
+    private Node head;
+    private Node tail;
+    private int length;
 
+    //create new node
+    public LinkedList(Integer value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    //create new node + add Node to the end
+    public void append(int value) {
+        Node newNode = new Node(value);
+
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+            length++;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+        length++;
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) {
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+
+        if (index == 0) {
+            return removeFirst();
+        }
+
+        if (index == length - 1) {
+            return removeLast();
+        }
+
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+
+    }
+
+    //create new node + add Node to beginning
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value) {
+        if (index < 0 || index >= length) {
+            return false;
+        }
+
+        Node current = head;
+        for (int i = 0; i != index; i++) {
+            current = current.next;
+        }
+        current.value = value;
+        return true;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) {
+            return false;
+        }
+
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+
+        if (index == length) {
+            append(value);
+            return true;
+        }
+
+        Node newNode = new Node(value);
+        Node current = get(index - 1);
+
+        newNode.next = current.next;
+        current.next = newNode;
+        length++;
+        return true;
+    }
+
+    public void reverse() {
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node after = temp.next;
+        Node before = null;
+
+        for (int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+    }
 
     public void add(int data) {
         Node newNode = new Node(data);
@@ -11,44 +172,26 @@ public class LinkedList {
         if (head == null) {
             head = newNode;
         } else {
-            while (current.getNext() != null) {
-                current = current.getNext();
+            while (current.next != null) {
+                current = current.next;
             }
-            current.setNext(newNode);
+            current.next = newNode;
         }
     }
 
     public void addFirst(int data) {
         Node newNode = new Node(data);
-        newNode.setNext(head);
+        newNode.next = head;
         head = newNode;
     }
 
+    public class Node {
+        int value;
+        Node next;
 
-    public void printValues() {
-        Node current = head;
-
-        while (current != null) {
-            System.out.println(current.getValue());
-            current = current.getNext();
-
-        }
-    }
-
-    public void delete(int data) {
-        Node current = head;
-        // 5    9   78    3
-        if (head.getValue() == data) {
-            head = head.getNext();
-            return;
-        }
-
-        while (current.getNext() != null && current.getNext().getValue() != data) {
-            current = current.getNext();
-        }
-
-        if (current.getNext() != null) {
-            current.setNext(current.getNext().getNext());
+        public Node(int value) {
+            this.value = value;
+            this.next = null;
         }
     }
 }
